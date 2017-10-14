1124da155ece6483643d43db35dd6a4983fecd3d
DESKTOP-KBUNFN3\hirot <hiroto7665@gmail.com>
2017-10-14 21:18:38 +0900

    add model

--- gunosynews/models.py
+++ gunosynews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Gunosy(models.Model):
+    article_title = models.CharField(max_length=100)
+    article_category = models.CharField(max_length=10)
