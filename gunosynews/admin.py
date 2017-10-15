df7fc77a5b3977a047fd56eca854fdc422150987
DESKTOP-KBUNFN3\hirot <hiroto7665@gmail.com>
2017-10-15 19:10:26 +0900

    add templates

--- gunosynews/admin.py
+++ gunosynews/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from gunosynews.models import Gunosy
+
+admin.site.register(Gunosy)
--- gunosynews/views.py
+++ gunosynews/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from gunosynews.models import Gunosy
 # Create your views here.
+
+def index(request):
+    article_title = Gunosy.objects.all()
+    context = {
+        'article_title': article_title
+    }
+    return render(request, 'gunosynews/index.html', context)
